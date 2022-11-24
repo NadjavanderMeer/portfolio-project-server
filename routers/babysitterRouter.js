d4d02481e2598c085db7b1863cc44ecc59875735
NadjavanderMeer <nadjavandermeer@gmail.com>
2022-11-24 14:46:40 +0100

    created endpoint for babysitter details page

--- routers/babysitterRouter.js
+++ routers/babysitterRouter.js
@@ -4,11 +4,25 @@ const Babysitter = require("../models").profile;
 
 const router = new Router();
 
+// display list of babysitters
 router.get("/", async (request, response, next) => {
   const babysitters = await Babysitter.findAll({
     where: { isBabysitter: true },
   });
   response.send(babysitters);
+});
+
+// view babysitter profile
+router.get("/:id", async (request, response, next) => {
+  const babysitterId = parseInt(request.params.id);
+  console.log("babysitter ID", babysitterId);
+  const babysitter = await Babysitter.findByPk(babysitterId);
+
+  if (!babysitter) {
+    response.status(404).send("This profile does not exist");
+  } else {
+    response.send(babysitter);
+  }
 });
 
 module.exports = router;
--- routers/familyRouter.js
+++ routers/familyRouter.js
@@ -4,6 +4,7 @@ const Family = require("../models").profile;
 
 const router = new Router();
 
+// display list of families
 router.get("/", async (request, response, next) => {
   const families = await Family.findAll({
     where: { isBabysitter: false },
